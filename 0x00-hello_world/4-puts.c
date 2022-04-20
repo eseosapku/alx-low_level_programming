eaebb2f418a469388fbd2bf233d01a0dbe024bf0
eseosapku <eseosapku@gmail.com>
2022-04-20 10:07:00 -0700

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like buildin amultilingual puzzle");
+	return(0);
+}
