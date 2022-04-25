b13e2a8c0f1ecdbbc2b968c1ab3b99cd3cc947e0
eseosapku <eseosapku@gmail.com>
2022-04-25 12:20:59 -0700

    commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,17 +9,6 @@
  */
 
 int main(void)
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
 {
 	int d;
 
